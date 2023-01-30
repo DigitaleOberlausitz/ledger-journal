2e72e22a9a8bc642e88d5a55ecbf8f1c7af2d7a0
Anja Schaffhirt <anja.schaffhirt@digitale-oberlausitz.eu>
2023-01-30 16:21:39 +0000

    Update job config for HTML generator

--- .space.kts
+++ .space.kts
@@ -8,4 +8,14 @@ job("Print balance") {
     container(displayName = "ledger-cli", image = "dcycle/ledger:1") {
         args("-f", "2023.dat", "bal")
     }
+    container(displayName = "ledger-journal", image = "dcycle/ledger:1") {
+        shellScript {
+            content = "ledger -f 2023.dat xml > ${'$'}JB_SPACE_FILE_SHARE_PATH/journal.xml"
+        }
+    }
+    container(displayName = "xsltproc", image = "s3v1/xsltproc") {
+        shellScript {
+            content = "xsltproc -o ${'$'}JB_SPACE_FILE_SHARE_PATH/journal.html ledger-journal.xslt ${'$'}JB_SPACE_FILE_SHARE_PATH/journal.xml"
+        }
+    }
 }
