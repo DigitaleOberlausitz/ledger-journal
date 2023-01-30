0bfb166a3011f78682df498d2ceaef4143b4703e
Anja Schaffhirt <anja.schaffhirt@digitale-oberlausitz.eu>
2023-01-30 16:42:28 +0000

    Try to deploy something

--- .space.kts
+++ .space.kts
@@ -23,4 +23,15 @@ job("Print balance") {
             content = "ls -al; ls -al ${'$'}JB_SPACE_FILE_SHARE_PATH/; cat ${'$'}JB_SPACE_FILE_SHARE_PATH/journal.html"
         }
     }
+    container("openjdk:11") {
+        kotlinScript { api ->
+            api.space().projects.automation.deployments.start(
+                project = api.projectIdentifier(),
+                targetIdentifier = TargetIdentifier.Key("journal"),
+                version = "1.0.0",
+                // automatically update deployment status based on a status of a job
+                syncWithAutomationJob = true
+            )
+        }
+    }
 }
