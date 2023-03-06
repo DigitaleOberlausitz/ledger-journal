3ec7a06715a5d5c1c7b8da2f66db984432cae283
Anja Schaffhirt <anja.schaffhirt@digitale-oberlausitz.eu>
2023-03-06 21:05:51 +0000

    Revert "Change job to rebuild past years"
    This reverts commit c716c5386af7841644f171f29947b5382cfb98bd.

--- .space.kts
+++ .space.kts
@@ -17,11 +17,6 @@ job("Upload journal") {
     container(displayName = "ledger-journal", image = "dcycle/ledger:1") {
         shellScript {
             content = """
-                ledger -y %F -f "2018.dat" xml > "${'$'}JB_SPACE_FILE_SHARE_PATH/2018.xml"
-                ledger -y %F -f "2019.dat" xml > "${'$'}JB_SPACE_FILE_SHARE_PATH/2019.xml"
-                ledger -y %F -f "2020.dat" xml > "${'$'}JB_SPACE_FILE_SHARE_PATH/2020.xml"
-                ledger -y %F -f "2021.dat" xml > "${'$'}JB_SPACE_FILE_SHARE_PATH/2021.xml"
-                ledger -y %F -f "2022.dat" xml > "${'$'}JB_SPACE_FILE_SHARE_PATH/2022.xml"
                 ledger -y %F -f "${'$'}(date +%Y).dat" xml > "${'$'}JB_SPACE_FILE_SHARE_PATH/${'$'}(date +%Y).xml"
             """
         }
@@ -29,11 +24,6 @@ job("Upload journal") {
     container(displayName = "xsltproc", image = "s3v1/xsltproc") {
         shellScript {
             content = """
-                xsltproc -o "${'$'}JB_SPACE_FILE_SHARE_PATH/2018.html" ledger-journal.xslt "${'$'}JB_SPACE_FILE_SHARE_PATH/2018.xml"
-                xsltproc -o "${'$'}JB_SPACE_FILE_SHARE_PATH/2019.html" ledger-journal.xslt "${'$'}JB_SPACE_FILE_SHARE_PATH/2019.xml"
-                xsltproc -o "${'$'}JB_SPACE_FILE_SHARE_PATH/2020.html" ledger-journal.xslt "${'$'}JB_SPACE_FILE_SHARE_PATH/2020.xml"
-                xsltproc -o "${'$'}JB_SPACE_FILE_SHARE_PATH/2021.html" ledger-journal.xslt "${'$'}JB_SPACE_FILE_SHARE_PATH/2021.xml"
-                xsltproc -o "${'$'}JB_SPACE_FILE_SHARE_PATH/2022.html" ledger-journal.xslt "${'$'}JB_SPACE_FILE_SHARE_PATH/2022.xml"
                 xsltproc -o "${'$'}JB_SPACE_FILE_SHARE_PATH/${'$'}(date +%Y).html" ledger-journal.xslt "${'$'}JB_SPACE_FILE_SHARE_PATH/${'$'}(date +%Y).xml"
             """
         }
@@ -41,11 +31,6 @@ job("Upload journal") {
     container("alpine/curl") {
         shellScript {
             content = """
-                curl -i -H "Authorization: Bearer ${'$'}JB_SPACE_CLIENT_TOKEN" -F file=@"${'$'}JB_SPACE_FILE_SHARE_PATH/2018.html" "https://files.pkg.jetbrains.space/doev/p/finanzen/journals/"
-                curl -i -H "Authorization: Bearer ${'$'}JB_SPACE_CLIENT_TOKEN" -F file=@"${'$'}JB_SPACE_FILE_SHARE_PATH/2019.html" "https://files.pkg.jetbrains.space/doev/p/finanzen/journals/"
-                curl -i -H "Authorization: Bearer ${'$'}JB_SPACE_CLIENT_TOKEN" -F file=@"${'$'}JB_SPACE_FILE_SHARE_PATH/2020.html" "https://files.pkg.jetbrains.space/doev/p/finanzen/journals/"
-                curl -i -H "Authorization: Bearer ${'$'}JB_SPACE_CLIENT_TOKEN" -F file=@"${'$'}JB_SPACE_FILE_SHARE_PATH/2021.html" "https://files.pkg.jetbrains.space/doev/p/finanzen/journals/"
-                curl -i -H "Authorization: Bearer ${'$'}JB_SPACE_CLIENT_TOKEN" -F file=@"${'$'}JB_SPACE_FILE_SHARE_PATH/2022.html" "https://files.pkg.jetbrains.space/doev/p/finanzen/journals/"
                 curl -i -H "Authorization: Bearer ${'$'}JB_SPACE_CLIENT_TOKEN" -F file=@"${'$'}JB_SPACE_FILE_SHARE_PATH/${'$'}(date +%Y).html" "https://files.pkg.jetbrains.space/doev/p/finanzen/journals/"
             """
         }
